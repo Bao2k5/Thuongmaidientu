25f684b4e503acfe4c2b92525c984f88d2d47c36
Bao2k5 <leduongbao2019@gmail.com>
2025-11-03 12:48:41 +0700

    fix(api): add /api to baseURL and use /upload/image endpoint

--- FE/src/services/api.js
+++ FE/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { storage } from '../utils/helpers';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 // Create axios instance
 const api = axios.create({
