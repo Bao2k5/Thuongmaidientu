4fc827b3cfd35ea7ecb5ef1c729d81f86a1a3ef7
Bao2k5 <leduongbao2019@gmail.com>
2025-11-10 17:45:22 +0700

    Fix: xóa duplicate sortObject và sửa import paymentService

--- FE/src/pages/MomoSimulator.jsx
+++ FE/src/pages/MomoSimulator.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { paymentService } from '../services/api';
+import paymentService from '../services/paymentService';
 
 const MomoSimulator = () => {
   const [searchParams] = useSearchParams();
